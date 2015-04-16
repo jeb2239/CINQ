b6fd46dadf27beaefc71e79a3ef3820f10b5d8c6
Jonathan Barrios <jeb2239@columbia.edu>
2015-04-16 19:54:06 -0400

    doxygen script

---
+++ src/makeDocs.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import os
+from os.path import isfile
+from subprocess import call
+
+def commentReqs(filename):
+    fp=open(filename,'r')
+    modLines=[]
+    lines=fp.readlines()
+    for line in lines:
+        if "requires" not in line.strip().split(' ')[0]:
+            modLines.append(line)
+        elif '///' in line and '//' in line:
+            modLines.append(line)
+        else:
+            modLines.append("///"+line)
+
+    os.chdir('docs')
+    nfd=open(filename,"w")
+    nfd.writelines(modLines)
+    os.chdir('../')
+
+def copyDoxy(filename):
+    fp=open(filename,'r')
+    modLines=[]
+    lines=fp.readlines()
+    for line in lines:
+            modLines.append(line)
+    os.chdir('docs')
+    nfd=open(filename,"w")
+    nfd.writelines(modLines)
+    os.chdir('../')
+        
+
+def eachSrc():
+    try:
+     os.mkdir('docs')
+    except(OSError):
+        pass
+    
+    filenames=[]
+
+    for (dirpath, dirnames, f) in os.walk('./'):
+        filenames.append(f)
+        break
+
+    for name in filenames[0]:
+        if 'Doxyfile' in name : copyDoxy(name)
+        if '.cpp' in name: commentReqs(name)
+        if '.hpp' in name: commentReqs(name)
+    os.chdir('docs')
+
+    call("doxygen")
+
+eachSrc()
